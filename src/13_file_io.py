a49c224904de89e7b01a3a0856e4eae647c5a964
shaunmcarmody <shaun.carmody@live.co.uk>
2019-09-30 16:03:19 +0100

    13 complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+  read_data = f.read()
+  print(read_data)
+f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,6 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+  f.write('Sentence 1\nSentence 2\nSentence 3')
+f.closed
